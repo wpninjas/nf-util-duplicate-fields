f7b232c0d68d4cafb12a35472bd02039a54cfe91
Kyle B. Johnson <kyle@wpninjas.com>
2016-10-12 15:48:04 -0400

    update admin notice.

--- src/Notice.php
+++ src/Notice.php
@@ -11,17 +11,14 @@ final class NF_Util_DuplicateFields_Notice
     {
         $title = 'Ninja Forms - [Utility] Duplicate Fields';
 
-        if( 'complete' == get_option( 'nf_util_remove_duplicate_fields' ) ){
-            $classes = 'notice notice-success is-dismissible';
-            $message = 'Duplicate fields have been removed.';
+        if( get_option( 'nf_util_remove_duplicate_fields' ) ) {
+            $classes = 'notice notice-info is-dismissible';
+            $message = 'Duplicate fields are being removed in the background.';
             $link = array(
                 'href' => add_query_arg( array( 'nf_util_remove_duplicate_fields' => 'start' ), admin_url() ),
                 'text' => 'Re-start Process',
                 'classes' => 'button button-primary'
             );
-        } elseif( 'started' == get_option( 'nf_util_remove_duplicate_fields' ) ) {
-            $classes = 'notice notice-info is-dismissible';
-            $message = 'Duplicate fields are being removed. You will be notified when the process is complete.';
         } else {
             $classes = 'notice notice-warning is-dismissible';
             $message = 'If you are experiencing duplicate field data, click the button below to fix it.';
