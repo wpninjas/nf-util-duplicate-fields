6b1177987ed8261fa45c9f75a4b87cf36724d1e0
Kyle B. Johnson <kyle@wpninjas.com>
2016-10-12 15:47:50 -0400

    update form cache.

--- src/Process.php
+++ src/Process.php
@@ -11,15 +11,25 @@ class NF_Util_DuplicateFields_Process extends WP_Background_Process
         usort( $fields, array( $this, 'sort_fields' ) );
 
         $unique_keys = array();
+        $field_cache = array();
         foreach( $fields as $field ){
 
             $field_key = $field->get_setting( 'key' );
 
             if( ! in_array( $field_key, $unique_keys ) ){
                 $unique_keys[] = $field_key;
+                $field_cache[] = array(
+                    'id' => $field->get_id(),
+                    'settings' => $field->get_settings()
+                );
             } else {
                 $field->delete();
             }
+        }
+
+        if( $form_cache = get_option( 'nf_form_' . $form->get_id(), false ) ){
+            $form_cache[ 'fields' ] = $field_cache;
+            update_option( 'nf_form_' . $form->get_id(), $form_cache );
         }
 
         return false;
